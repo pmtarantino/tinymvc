5de8aa1e7e11f45143c2e7776c9478138e71e219
Patricio Tarantino <patriciotarantino@gmail.com>
2017-01-21 10:44:29 -0300

    Routing with parameters

--- src/Controller.php
+++ src/Controller.php
@@ -22,6 +22,8 @@ class Router{
 
 	public static function serve(){
 		$instance = self::get_instance();
+		$called = false;
+
 		if(array_key_exists('url', $_GET)){
 			// There is something in the URL
 			$endpoint = rtrim($_GET['url'],'/'); // Remove last dash if is in there
@@ -36,17 +38,31 @@ class Router{
 			$instance->use_handler($handler);
 
 		} else {
-			if(array_key_exists('404', $instance->config)){
-				$handler = $instance->config['404'];
-				$instance->use_handler($handler);
-			} else {
-				trigger_error("404 Error: Endpoint not configured", E_USER_ERROR);
+			// Check if matches with an endpoint with parameters.
+
+			foreach($instance->routes as $pattern => $handler){
+				$pattern = strtr($pattern, array(':param:' => '([a-zA-Z0-9-_]+)'));
+				if (preg_match('#^/?' . $pattern . '/?$#', $endpoint, $matches)) {
+					$params = array_splice($matches,1);
+					$instance->use_handler($handler, $params);
+					$called = true;
+					break;
+				}
+			}
+
+			if(!$called){
+				if(array_key_exists('404', $instance->config)){
+					$handler = $instance->config['404'];
+					$instance->use_handler($handler);
+				} else {
+					trigger_error("404 Error: Endpoint not configured", E_USER_ERROR);
+				}
 			}
 		}
 
 	}
 
-	private static function use_handler($handler){
+	private static function use_handler($handler, $params=array()){
 		if(!class_exists($handler)){
 			trigger_error("Handler undefined", E_USER_ERROR);
 		}
@@ -56,7 +72,7 @@ class Router{
 			trigger_error("Method undefined for handler", E_USER_ERROR);
 		}
 
-		call_user_func(array($handler, $method));
+		call_user_func(array($handler, $method), $params);
 	}
 
 	public static function get_instance() {
